658fa55ab323b7f30307f2c818da512f26367a13
bngandan <bngandan@gmail.com>
2020-09-21 06:10:04 +0800

    week13 hw2

---
+++ homeworks/week13/hw2/src/api.js
@@ -0,0 +1,29 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable quotes */
+/* eslint-disable  eol-last */
+/* eslint-disable  import/no-unresolved */
+
+import $ from "jquery"
+
+export function getComments(apiUrl, siteKey, before, cb) {
+  let url = `${apiUrl}/api_comments.php?site_key=${siteKey}`
+  if (before) {
+    url += `&before=${before}`;
+  }
+  $.ajax({
+    url,
+  }).done((data) => {
+    cb(data);
+  });
+}
+
+export function addComments(apiUrl, siteKey, data, cb) {
+  $.ajax({
+    type: 'POST',
+    url: `${apiUrl}/api_add_comments.php`,
+    data,
+  }).done((value) => {
+    cb(value);
+  });
+}
